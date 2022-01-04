8308632c348f3a265226b7e3160a93623dfeac53
Daehyun Cho <62973169+1pha@users.noreply.github.com>
2022-01-04 16:46:21 +0900

    Update update_md.sh

--- .autoupdate/update_md.sh
+++ .autoupdate/update_md.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 output=($(python3 count.py | tr -d '[],'))
-statement=$(echo "총 ${output[0]}개의 문제와 ${output[1]}개의 해답이 업로드되었습니다.")
+statement=$(echo "**총 ${output[0]}개의 문제와 ${output[1]}개의 해답이 업로드되었습니다.**")
 
 sed '3d' README.md | sed "2a\\${statement}" >> NEW.md
 rm README.md
